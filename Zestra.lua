3840e19a8e8262cf15911ff78138eb9b695adde4
Cat-Ware <72982537+PandaCyphy@users.noreply.github.com>
2021-06-25 23:17:17 +0530

    Update Zestra.lua

--- Zestra.lua
+++ Zestra.lua
@@ -1,1 +1,8 @@
-info"not out yet"
+--keeping this uhh open source cause your a skid :troll:
+--notoriety soon
+
+if game.PlaceId == 1962086868 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/PandaCyphy/Zestra-Hub/main/Games/TowerOfHell.lua")())
+else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/PandaCyphy/Zestra-Hub/main/Games/Universal.lua")())
+end    
